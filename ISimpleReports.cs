bf6a02ac392770ced08060441935fe00b0c5bd53
Влад Логунов <vl3012@icloud.com>
2021-08-16 23:11:40 +0700

    Уменьшено количество строк в методах SimpleReports

--- ISimpleReports.cs
+++ ISimpleReports.cs
@@ -49,68 +49,45 @@ namespace ClassLibrary
 
         public async Task<string> Dashboard(string @base, string domain)
         {
-            string Url = "domain_dashboard";
-           
-            return baseUrl + Url + "?base=" + @base + "&domain=" + domain;
+            return baseUrl + "domain_dashboard" + "?base=" + @base + "&domain=" + domain;
         }
         public async Task<string> DashboardKey(string @base, string keyword)
         {
-            string Url = "domain_dashboard";
-
-            return baseUrl + Url + "?base=" + @base + "&domain=" + keyword;
+            return baseUrl + "domain_dashboard" + "?base=" + @base + "&domain=" + keyword;
         }
-        
         public async Task<string> KeyWordObyavlenia(string @base, string domain, int ads_id)   //?????????????????????
         {
-            string Url = "context/keywords/byads";
-
-            return baseUrl + Url + "?base=" + @base + "&domain=" + domain;
+            return baseUrl + "context/keywords/byads" + " ? base=" + @base + "&domain=" + domain;
         }
         public async Task<string> Advertisement(string @base, string domain, bool full)
         {
-            string Url = "report/simple/context/ads";
-
-            return baseUrl + Url + "?base=" + @base + "&domain=" + domain;
+            return baseUrl + "context/ads" + "?base=" + @base + "&domain=" + domain;
         }
         public async Task<string> UFact(string @base, string domain)
         {
-            string Url = "report/simple/context/ads/facts";
-
-            return baseUrl + Url + "?base=" + @base + "&domain=" + domain;
+            return baseUrl + "context/ads/facts" + "?base=" + @base + "&domain=" + domain;
         }
         public async Task<string> ULink(string @base, string domain)
         {
-            string Url = "report/simple/context/ads/links";
-
-            return baseUrl + Url + "?base=" + @base + "&domain=" + domain;
+            return baseUrl + "context/ads/links" + "?base=" + @base + "&domain=" + domain;
         }
-
         public async Task<string> SitePage(string @base, string domain)
         {
-            string Url = "report/simple/organic/sitepages";
-
-            return baseUrl + Url + "?base=" + @base + "&domain=" + domain;
+            return baseUrl + "organic/sitepages" + "?base=" + @base + "&domain=" + domain;
         }
         public async Task<string> KeyWordPage(string @base, string domain, string page_url)
         {
-            string Url = "report/simple/organic/keywords/bypage?";
-
-            return baseUrl + Url + "?base=" + @base + "&domain=" + domain + "&page_url=" + page_url;
+            return baseUrl + "organic/keywords/bypage?" + "?base=" + @base + "&domain=" + domain + "&page_url=" + page_url;
         }
         public async Task<string> PageConcurents(string @base, string domain, string page_url)
         {
-            string Url = "report/simple/organic/concurent_pages";
-
-            return baseUrl + Url + "?base=" + @base + "&domain=" + "&page_url=" + page_url;
+            return baseUrl + "organic/concurent_pages" + "?base=" + @base + "&domain=" + "&page_url=" + page_url;
         }
-
         public async Task<string> KeyWord(string context, string @base, string domain)
         {
             if (context == "context" || context == "organic")
             {
-                string Url = baseUrl + context + "/keywords";
-
-                return Url + "?base=" + @base + "&domain=" + domain; 
+                return baseUrl + context + "/keywords" + "?base=" + @base + "&domain=" + domain; 
             }
             else
             {
@@ -119,23 +96,15 @@ namespace ClassLibrary
         }
         public async Task<string> Concurents(string @base, string domain)
         {
-            string Url = "organic/concurents";
-
-            return baseUrl + Url + "?base=" + @base + "&domain=" + domain;;
+            return baseUrl + "organic/concurents" + "?base=" + @base + "&domain=" + domain;;
         }
-
         public async Task<string> Owner(string id, string mode)
         {
-            string Url = "report/owner/";
-
-            return Url + mode + "?id=" + id;
-
+            return "report/owner/" + mode + "?id=" + id;
         }
         public async Task<string> Similarkeys(string @base, string keyword)
         {
-            string Url = "similarkeys";
-
-            return  baseUrl + Url + "?base=" + @base + "&domain=" + keyword;;
+            return  baseUrl + "similarkeys" + "?base=" + @base + "&domain=" + keyword;;
         }
 
 
